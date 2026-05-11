9bf1a47f9730da87c77d1e2fe99e3546c635ba45
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-05-11 15:33:56 +0800

    gpu: msm: trivial whitespace fix
    Change-Id: I4cfb4a61e3bf175ad73f03bcc4418cd67623cb5a
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2606,7 +2606,7 @@ static int kgsl_setup_anon_useraddr(struct kgsl_pagetable *pagetable,
 		entry->memdesc.gpuaddr = (uint64_t) hostptr;
 	}
 
-	ret =  memdesc_sg_virt(&entry->memdesc, hostptr);
+	ret = memdesc_sg_virt(&entry->memdesc, hostptr);
 
 	if (ret && kgsl_memdesc_use_cpu_map(&entry->memdesc))
 		kgsl_mmu_put_gpuaddr(&entry->memdesc);
